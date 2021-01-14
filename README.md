57761a94869a02e3ac3038f8b5913ffe97c679c3
Chelsea Houston <chelseahouston29@gmail.com>
2021-01-14 13:55:11 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Convert String to Integar
+A Ruby program to convert a string in to an integer and then perform any mathematical equation with it.
+
+## Installation
+No installation required to run this project.
+```
+Ruby main.cpp
+```
+
+Created as part of TechTalent Academy Women in Software course.
---
+++ main.rb
@@ -0,0 +1,14 @@
+# Declare String ---
+string01 = "118118"
+puts "String = #{string01}"
+
+# Convert String to Integer ---
+string01 = string01.to_i
+
+# Mathematical Equations with Int ---
+puts "String x 5 ="
+puts string01 * 5
+puts "String + String ="
+puts string01 + string01
+puts "String to the power of 8 ="
+puts string01**8
